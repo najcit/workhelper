1ab7b685904eeaa0b6785f189ed54726274d5a3a
lidajun <18321965@985@com>
2023-01-27 21:01:18 +0800

    add shortcut key

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import copy
 import os
 import random
 import re
+import time
+
 import PySimpleGUI as sg
 import psgtray as pt
 import math
@@ -14,8 +16,42 @@ import winshell
 import yaml
 from appupdater import AppUpdater
 
-THREAD_KEY = '-THREAD-'
-DL_END_KEY = '-DOWNLOAD-END-'
+ALL_TAG = '全部应用'
+EVENT_THREAD_KEY = '-THREAD-'
+EVENT_DL_END_KEY = '-DOWNLOAD-END-'
+EVENT_SHOW = '显示界面'
+EVENT_HIDE = '隐藏界面'
+EVENT_QUIT = '退出'
+EVENT_QUIT_WITH_HOTKEY = '退出(&Q)'
+EVENT_FILE = '文件(&F)'
+EVENT_NEW_WINDOW = '新建窗口(&N)'
+EVENT_CLOSE_WINDOW = '关闭窗口(&E)'
+EVENT_IMPORT_APP_INFO = '导入应用信息(&I)'
+EVENT_EXPORT_APP_INFO = '导出应用信息(&X)'
+EVENT_EDIT = '编辑(&E)'
+EVENT_MANAGE_TAG = '管理标签(&M)'
+EVENT_ADD_TAG = '新建标签(&N)'
+EVENT_MOD_TAG = '编辑标签'
+EVENT_VIEW = '视图(&V)'
+EVENT_SHOW_ICON = '图标(&G)'
+EVENT_SHOW_LIST = '列表(&L)'
+EVENT_OPTION = '选项(&S)'
+EVENT_SELECT_ROOT = '选择目录(&O)'
+EVENT_SELECT_THEME = '选择主题(&T)'
+EVENT_SELECT_FONT = '选择字体(&F)'
+EVENT_SET_WINDOW = '设置窗口(&W)'
+EVENT_HELP = '帮助(&H)'
+EVENT_CHECK_UPDATE = '检查更新(&U)'
+EVENT_ABOUT = '关于(&A)'
+EVENT_RMV_TAG = '删除标签'
+EVENT_REFRESH = '刷新'
+EVENT_ADD_APP = '添加应用'
+EVENT_MOD_APP = '修改应用'
+EVENT_RMV_APP = '删除应用'
+EVENT_RUN_APP = '运行应用'
+EVENT_OPEN_APP_PATH = '打开应用路径'
+EVENT_COPY_APP_PATH = '复制应用路径'
+EVENT_SEARCH = '搜索'
 
 
 class MyApp:
@@ -31,6 +67,7 @@ class MyApp:
     filter_cond = None
     apps_info = None
     event_functions = None
+    event_hotkeys = None
 
     def __init__(self, root, theme, enable_systray, enable_env, location, name=None):
         self.name = name if name is not None else '工作助手'
@@ -42,31 +79,9 @@ class MyApp:
         self.enable_env = enable_env
         self.location = location
         self.updater = AppUpdater()
-        self.event_functions = {
-            sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED: self.show_top,
-            sg.WIN_CLOSE_ATTEMPTED_EVENT: self.attempt_exit,
-            '显示界面': self.show_top,
-            '隐藏界面': self.attempt_exit,
-            '关闭窗口': self.attempt_exit,
-            '退出': self.exit,
-            '检查更新': self.check_update,
-            '关于': self.about,
-            '新建窗口': self.new_window,
-            '导出应用信息': self.export_app,
-            '打开目录': self.select_app_root,
-            '选择主题': self.set_theme,
-            '管理标签': self.manage_tab,
-            '新建标签': self.add_tab,
-            '修改标签': self.mod_tab,
-            '删除标签': self.rmv_tab,
-            '刷新': self.refresh,
-            '新建应用': self.add_app,
-            '修改应用': self.mod_app,
-            '删除应用': self.rmv_app,
-            '打开应用路径': self.open_app,
-            '复制应用路径': self.copy_app_path,
-            '搜索': self.search_app,
-        }
+        self.apps_info = self.init_apps_info()
+        self.event_functions = self.init_event_functions()
+        self.event_hotkeys = self.init_event_hotkeys()
 
     @staticmethod
     def show(root, theme, enable_systray, enable_env, location=None):
@@ -85,7 +100,7 @@ class MyApp:
         self.init_systray()
         return self
 
-    def refresh(self, root=None, theme=None, filter_cond=None):
+    def refresh(self, root=None, theme=None, filter_cond=None, **_kwargs):
         if root and root != self.root:
             self.root = root
             self.set_env('MY_ROOT', self.root)
@@ -100,6 +115,7 @@ class MyApp:
     def run(self):
         while True:
             event, values = self.read()
+            print(event, values, isinstance(event, tuple), type(event), len(event) > 1)
             if event in self.event_functions and callable(self.event_functions[event]):
                 # noinspection PyArgumentList
                 self.event_functions[event](event=event, values=values)
@@ -107,7 +123,7 @@ class MyApp:
                 print(event, values, isinstance(event, tuple), type(event), len(event) > 1)
                 if isinstance(event, str) and event.endswith('Click'):
                     self.select_app(event=event)
-                elif isinstance(event, tuple) and event == (THREAD_KEY, DL_END_KEY):
+                elif isinstance(event, tuple) and event == (EVENT_THREAD_KEY, EVENT_DL_END_KEY):
                     print(event, values)
                     self.exit()
                 else:
@@ -115,36 +131,24 @@ class MyApp:
 
     def init_window(self):
         sg.theme(self.theme)
-        menu = [['文件', ['新建窗口', '关闭窗口', '导入应用信息', '导出应用信息', '退出']],
-                ['编辑', ['管理标签', '新建标签']],
-                ['设置', ['打开目录', '选择主题']],
-                ['帮助', ['检查更新', '关于']]]
-        layout = [[sg.MenubarCustom(menu)]]
-        layout += [sg.Input(enable_events=True, key='-CONTENT-', expand_x=True),
-                   sg.Button('搜索', bind_return_key=True)],
-        whole = '全部应用'
-        apps_info = {whole: []}
-        for parent, directorys, _files in os.walk(self.root):
-            sub_root = parent.replace(self.root + os.path.sep, '')
-            if parent != self.root and sub_root.find(os.path.sep) == -1:
-                if parent not in apps_info:
-                    apps_info[parent] = []
-                apps = [{
-                    'name': directory,
-                    'path': os.path.join(parent, directory),
-                    'icon': os.path.join(os.getcwd(), 'images', 'robot_' + str(random.randrange(1, 6)) + '.png'),
-                } for directory in directorys]
-                apps_info[parent] += apps
-                apps_info[whole] += apps
-        self.apps_info = apps_info
+        menu_def = [[EVENT_FILE, [EVENT_NEW_WINDOW, EVENT_CLOSE_WINDOW, EVENT_IMPORT_APP_INFO,
+                                  EVENT_EXPORT_APP_INFO, EVENT_QUIT_WITH_HOTKEY]],
+                    [EVENT_EDIT, [EVENT_MANAGE_TAG, EVENT_ADD_TAG]],
+                    [EVENT_VIEW, [EVENT_SHOW_ICON, EVENT_SHOW_LIST]],
+                    [EVENT_OPTION, [EVENT_SELECT_ROOT, EVENT_SELECT_THEME, EVENT_SELECT_FONT, EVENT_SET_WINDOW]],
+                    [EVENT_HELP, [EVENT_CHECK_UPDATE, EVENT_ABOUT]]]
+        layout = [[sg.Menu(menu_def, key='-MENUBAR-')]]
+        layout += [sg.Input(enable_events=False, key='-CONTENT-', expand_x=True),
+                   sg.Button(EVENT_SEARCH, bind_return_key=True)],
         filter_tab = self.filter_cond['tab'] if self.filter_cond and len(self.filter_cond) > 0 else ''
         filter_content = self.filter_cond['content'] if self.filter_cond and len(self.filter_cond) > 0 else ''
         tabs = []
         keys = []
-        max_num_per_row = max(math.floor(len(apps_info) / 2), 4)
-        app_right_click_menu = [['右击菜单'], ['执行应用', '修改应用', '删除应用', '打开应用路径', '复制应用路径']]
-        tab_right_click_menu = [['右击菜单'], ['修改标签', '删除标签', '新建应用', '刷新']]
-        for tab, apps in apps_info.items():
+        max_num_per_row = max(math.floor(len(self.apps_info) / 2), 4)
+        app_right_click_menu = [[], [EVENT_RUN_APP, EVENT_MOD_APP, EVENT_RMV_APP, EVENT_OPEN_APP_PATH,
+                                     EVENT_COPY_APP_PATH]]
+        tab_right_click_menu = [[], [EVENT_ADD_TAG, EVENT_RMV_TAG, EVENT_ADD_APP, EVENT_REFRESH]]
+        for tab, apps in self.apps_info.items():
             tab_name = os.path.basename(tab).ljust(4, ' ')
             tab_key = tab
             tab_layout = []
@@ -154,38 +158,34 @@ class MyApp:
                 app_name = app['name']
                 app_icon = app['icon']
                 app_key = tab + '#' + app['path']
-                button = sg.Button(button_text=app_name, tooltip=app_name, key=app_key, size=(18, 1), font=12,
+                button = sg.Button(button_text=app_name, tooltip=app_name, key=app_key, size=(20, 1),
                                    button_color=('black', 'white'), enable_events=True, border_width=0,
                                    right_click_menu=app_right_click_menu, expand_x=True, expand_y=True)
                 if os.path.isfile(app_icon):
-                    icon = sg.Image(source=app_icon, size=(64, 64), background_color='white')
+                    icon = sg.Image(source=app_icon, background_color='white')
                     col = sg.Column([[icon, button]], background_color='white')
                 else:
                     col = sg.Column([[button]], background_color='white')
-                if is_filter:
-                    if re.search(filter_content, app_name, re.IGNORECASE):
-                        row_layout.append(col)
-                        keys.append(app_key)
-                else:
+                if (is_filter and re.search(filter_content, app_name, re.IGNORECASE)) or not is_filter:
                     row_layout.append(col)
                     keys.append(app_key)
-
                 if len(row_layout) == max_num_per_row:
                     tab_layout.append(copy.deepcopy(row_layout))
                     row_layout.clear()
             else:
                 if len(row_layout) > 0:
                     tab_layout.append(row_layout)
-            tabs.append(sg.Tab(tab_name[:4], tab_layout, key=tab_key, right_click_menu=tab_right_click_menu))
+            tabs.append(sg.Tab(tab_name, tab_layout, key=tab_key, expand_x=True, expand_y=True,
+                               right_click_menu=tab_right_click_menu))
         layout += [[sg.TabGroup([tabs], key='-TAB-GROUP-', expand_x=True, expand_y=True,
                                 right_click_menu=tab_right_click_menu)]]
         layout += [[sg.Text('欢迎使用' + self.name, key='-NOTIFICATION-'), sg.Sizegrip()]]
         if self.location:
             self.window = sg.Window(self.name, layout, location=self.location, enable_close_attempted_event=True,
-                                    resizable=True, finalize=True, icon=self.icon)
+                                    resizable=True, finalize=True, icon=self.icon, font='Courier 12')
         else:
             self.window = sg.Window(self.name, layout, enable_close_attempted_event=True, resizable=True,
-                                    finalize=True, icon=self.icon)
+                                    finalize=True, icon=self.icon, font='Courier 12')
         self.window.set_min_size(self.window.size)
         if filter_tab:
             self.window[filter_tab].select()
@@ -194,10 +194,16 @@ class MyApp:
         for key in keys:
             self.window[key].bind('<Button-1>', ' LeftClick')
             self.window[key].bind('<Button-3>', ' RightClick')
+        self.bind_hotkeys()
         return self.window
 
+    def bind_hotkeys(self):
+        for event, hotkeys in self.event_hotkeys.items():
+            for hotkey in hotkeys:
+                self.window.bind(hotkey, event)
+
     def init_systray(self):
-        menu = ['后台菜单', ['显示界面', '隐藏界面', '退出']]
+        menu = [[], [EVENT_SHOW, EVENT_HIDE, EVENT_QUIT]]
         if self.enable_systray and self.window:
             self.systray = pt.SystemTray(menu, icon=self.icon, window=self.window, tooltip=self.name,
                                          single_click_events=False)
@@ -231,7 +237,7 @@ class MyApp:
         self.close()
         exit(0)
 
-    def set_theme(self, **_kwargs):
+    def select_theme(self, **_kwargs):
         cur_theme = self.theme
         layout = [[sg.Text('See how elements look under different themes by choosing a different theme here!')],
                   [sg.Listbox(values=sg.theme_list(), default_values=[cur_theme], size=(20, 12), key='THEME_LISTBOX',
@@ -266,7 +272,7 @@ class MyApp:
                 if percentage >= 100:
                     value = '最新版软件下载已完成。'
                     progressbar.update(value=value)
-                    self.window.write_event_value((THREAD_KEY, DL_END_KEY), percentage)
+                    self.window.write_event_value((EVENT_THREAD_KEY, EVENT_DL_END_KEY), percentage)
 
             self.window.start_thread(lambda: self.updater.install(update_progress_func=update_progress), ())
 
@@ -279,7 +285,7 @@ class MyApp:
         location = (location_x + 20, location_y + 20)
         MyApp.show(self.root, self.theme, self.enable_systray, self.enable_env, location)
 
-    def manage_tab(self):
+    def manage_tag(self, **_kwargs):
         values = [os.path.basename(tab) for tab, _ in self.apps_info.items()]
         cur_tab = os.path.basename(self.window['-TAB-GROUP-'].get())
         col1 = sg.Column([[sg.Listbox(values, key='-TAB-LIST-', default_values=[cur_tab],
@@ -315,7 +321,7 @@ class MyApp:
             elif event == '-OK-':
                 break
             elif event == '-ADD-':
-                new_tab = self.add_tab()
+                new_tab = self.add_tag()
                 print(type(new_tab))
                 if isinstance(new_tab, str):
                     values = window['-TAB-LIST-'].get_list_values()
@@ -326,7 +332,7 @@ class MyApp:
                 cur_tab = window['-TAB-LIST-'].get()[0]
                 print('cur_tab', cur_tab)
                 tab_key = os.path.join(self.root, cur_tab)
-                new_tab = self.mod_tab(tab_key)
+                new_tab = self.mod_tag(tab_key)
                 if isinstance(new_tab, str):
                     values = window['-TAB-LIST-'].get_list_values()
                     index = values.index(cur_tab)
@@ -335,7 +341,7 @@ class MyApp:
             elif event == '-DELETE-':
                 cur_tab = window['-TAB-LIST-'].get()[0]
                 tab_key = os.path.join(self.root, cur_tab)
-                if self.rmv_tab(tab_key):
+                if self.rmv_tag(tab_key):
                     values = window['-TAB-LIST-'].get_list_values()
                     values.remove(cur_tab)
                     window['-TAB-LIST-'].update(values)
@@ -352,7 +358,7 @@ class MyApp:
                 print(event, values)
         window.close()
 
-    def add_tab(self):
+    def add_tag(self, **_kwargs):
         result = False
         layout = [[sg.Text('新标签'), sg.Input(key='-NEW-TAB-')],
                   [sg.Button('确定'), sg.Button('取消')]]
@@ -374,7 +380,7 @@ class MyApp:
             self.refresh()
         return result
 
-    def mod_tab(self, tab_key=None, **kwargs):
+    def mod_tag(self, tab_key=None, **kwargs):
         if tab_key is None:
             tab_key = kwargs['values']['-TAB-GROUP-']
         result = False
@@ -399,7 +405,7 @@ class MyApp:
             self.refresh()
         return result
 
-    def rmv_tab(self, tab_key=None, **kwargs):
+    def rmv_tag(self, tab_key=None, **kwargs):
         if tab_key is None:
             tab_key = kwargs['values']['-TAB-GROUP-']
         tab_name = os.path.basename(tab_key)
@@ -415,7 +421,7 @@ class MyApp:
         key = event.replace('Click', '').replace('Right', '').replace('Left', '').strip()
         self.window[key].set_focus()
 
-    def search_app(self, **kwargs):
+    def search(self, **kwargs):
         values = kwargs['values']
         tab = values['-TAB-GROUP-']
         content = values['-CONTENT-']
@@ -499,7 +505,7 @@ class MyApp:
             self.refresh()
         return result
 
-    def open_app(self):
+    def open_app_path(self):
         item = self.window.find_element_with_focus()
         app_key = item.key
         cur_path = app_key.split('#')[1]
@@ -507,7 +513,7 @@ class MyApp:
         if os.path.exists(cur_path):
             os.startfile(cur_path)
 
-    def run_app(self):
+    def run_app(self, **_kwargs):
         item = self.window.find_element_with_focus()
         if str(item).find('Button') > -1:
             app_key = item.key
@@ -565,10 +571,88 @@ class MyApp:
             result = True
         return result
 
-    def select_app_root(self):
+    def select_root(self):
         root = sg.popup_get_folder(message='应用根目录', title='选择应用根目录', keep_on_top=True)
         if os.path.isdir(root):
             self.refresh(root)
+
+    def init_apps_info(self):
+        apps_info = {ALL_TAG: []}
+        for parent, directorys, _files in os.walk(self.root):
+            sub_root = parent.replace(self.root + os.path.sep, '')
+            if parent != self.root and sub_root.find(os.path.sep) == -1:
+                if parent not in apps_info:
+                    apps_info[parent] = []
+
+                def timestamp_to_datetime(time_stamp, format_string="%Y-%m-%d %H:%M:%S"):
+                    time_array = time.localtime(time_stamp)
+                    str_date = time.strftime(format_string, time_array)
+                    return str_date
+
+                apps = [{
+                    'name': directory,
+                    'path': os.path.join(parent, directory),
+                    'icon': os.path.join(os.getcwd(), 'images', 'robot_' + str(random.randrange(1, 6)) + '.png'),
+                    'time': timestamp_to_datetime(os.path.getmtime(os.path.join(parent, directory))),
+                } for directory in directorys]
+                apps_info[parent] += apps
+                apps_info[ALL_TAG] += apps
+        return apps_info
+
+    def init_event_functions(self):
+        event_func = {
+            sg.EVENT_SYSTEM_TRAY_ICON_DOUBLE_CLICKED: self.show_top,
+            sg.WIN_CLOSE_ATTEMPTED_EVENT: self.attempt_exit,
+            EVENT_SHOW: self.show_top,
+            EVENT_HIDE: self.attempt_exit,
+            EVENT_QUIT: self.exit,
+            EVENT_CLOSE_WINDOW: self.attempt_exit,
+            EVENT_QUIT_WITH_HOTKEY: self.exit,
+            EVENT_CHECK_UPDATE: self.check_update,
+            EVENT_ABOUT: self.about,
+            EVENT_NEW_WINDOW: self.new_window,
+            EVENT_IMPORT_APP_INFO: self.import_app,
+            EVENT_EXPORT_APP_INFO: self.export_app,
+            EVENT_SELECT_ROOT: self.select_root,
+            EVENT_SELECT_THEME: self.select_theme,
+            EVENT_SEARCH: self.search,
+            EVENT_REFRESH: self.refresh,
+            EVENT_MANAGE_TAG: self.manage_tag,
+            EVENT_ADD_TAG: self.add_tag,
+            EVENT_MOD_TAG: self.mod_tag,
+            EVENT_RMV_TAG: self.rmv_tag,
+            EVENT_ADD_APP: self.add_app,
+            EVENT_MOD_APP: self.mod_app,
+            EVENT_RMV_APP: self.rmv_app,
+            EVENT_RUN_APP: self.run_app,
+            EVENT_OPEN_APP_PATH: self.open_app_path,
+            EVENT_COPY_APP_PATH: self.copy_app_path,
+        }
+        new_event_func = {}
+        for key, value in event_func.items():
+            new_event_func[key] = value
+            if '&' in key:
+                new_event_func[key.replace('&', '')] = value
+        return new_event_func
+
+    @staticmethod
+    def init_event_hotkeys():
+        return {
+            EVENT_QUIT: ['<Control-q>', '<Control-Q>'],
+            EVENT_CLOSE_WINDOW: ['<Control-e>', '<Control-E>'],
+            EVENT_CHECK_UPDATE: ['<Control-u>', '<Control-U>'],
+            EVENT_ABOUT: ['<Control-a>', '<Control-A>'],
+            EVENT_NEW_WINDOW: ['<Control-n>', '<Control-N>'],
+            EVENT_IMPORT_APP_INFO: ['<Control-i>', '<Control-I>'],
+            EVENT_EXPORT_APP_INFO: ['<Control-x>', '<Control-X>'],
+            EVENT_SELECT_ROOT: ['<Control-o>', '<Control-O>'],
+            EVENT_SELECT_THEME: ['<Control-t>', '<Control-T>'],
+            EVENT_REFRESH: ['<F5>'],
+            EVENT_MANAGE_TAG: ['<Control-m>', '<Control-M>'],
+            EVENT_RUN_APP: ['<Control-r>', '<Control-R>'],
+            EVENT_OPEN_APP_PATH: ['<Control-p>', '<Control-P>'],
+            EVENT_COPY_APP_PATH: ['<Control-c>', '<Control-C>'],
+        }
 
     @staticmethod
     def set_env(key, value):
