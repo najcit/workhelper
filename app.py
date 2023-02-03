5faf81817b1ce34ad0f14a1d42a394f7393c7fd0
lidajun <18321965@985@com>
2023-02-03 22:30:16 +0800

    add sort view and sorted

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 import contextlib
 import copy
+import glob
 import os
 import random
 import re
@@ -14,6 +15,14 @@ import winshell
 import yaml
 from appupdater import AppUpdater
 from time import localtime, strftime
+from enum import Enum
+
+
+class SortType(Enum):
+    DEFAULT = 1
+    ASCEND = 2
+    DESCEND = 3
+
 
 ALL_TAG = '全部应用'
 E_THREAD_KEY = '-THREAD-'
@@ -32,8 +41,8 @@ E_MANAGE_TAG = '管理标签(&M)'
 E_ADD_TAG = '新建标签(&N)'
 E_MOD_TAG = '编辑标签'
 E_VIEW = '视图(&V)'
-E_SHOW_ICON = '图标(&G)'
-E_SHOW_LIST = '列表(&L)'
+E_SHOW_ICON = '图标显示(&G)'
+E_SHOW_LIST = '列表显示(&L)'
 E_OPTION = '选项(&S)'
 E_SELECT_ROOT = '选择目录(&O)'
 E_SELECT_THEME = '选择主题(&T)'
@@ -42,33 +51,19 @@ E_SET_WINDOW = '设置窗口(&W)'
 E_HELP = '帮助(&H)'
 E_CHECK_UPDATE = '检查更新(&U)'
 E_ABOUT = '关于(&A)'
-E_RMV_TAG = '删除标签'
-E_REFRESH = '刷新'
-E_ADD_APP = '添加应用'
-E_MOD_APP = '修改应用'
-E_RMV_APP = '删除应用'
-E_RUN_APP = '运行应用'
-E_OPEN_APP_PATH = '打开应用路径'
-E_COPY_APP_PATH = '复制应用路径'
+E_RMV_TAG = '删除标签(&D)'
+E_SRT_APP = '排序应用(&S)'
+E_ADD_APP = '添加应用(&A)'
+E_REFRESH = '刷新(&R)'
+E_MOD_APP = '修改应用(&M)'
+E_RMV_APP = '删除应用(&D)'
+E_RUN_APP = '运行应用(&R)'
+E_OPEN_APP_PATH = '打开应用路径(&O)'
+E_COPY_APP_PATH = '复制应用路径(&C)'
 E_SEARCH = '搜索'
 
 
 class MyApp:
-    name = None
-    version = None
-    root = None
-    theme = None
-    list_view = False
-    enable_systray = False
-    enable_env = False
-    window = None
-    systray = None
-    location = None
-    filter_cond = None
-    apps_info = None
-    app_keys = []
-    event_functions = None
-    event_hotkeys = None
 
     def __init__(self, root, theme, enable_systray, enable_env, location, name=None):
         self.name = name if name is not None else '工作助手'
@@ -76,10 +71,15 @@ class MyApp:
         self.icon = 'workhelper.ico'
         self.root = root
         self.theme = theme
-        self.list_view = False
         self.enable_systray = enable_systray
         self.enable_env = enable_env
         self.location = location
+        self.app_icons = []
+        self.list_view = False
+        self.sort_type = SortType.DEFAULT
+        self.filter_cond = None
+        self.window = None
+        self.systray = None
         self.updater = AppUpdater()
         self.apps_info = self.init_apps_info()
         self.event_functions = self.init_event_functions()
@@ -130,106 +130,6 @@ class MyApp:
                     self.exit()
                 else:
                     self.run_app()
-
-    def init_window_content(self, filter_tag, filter_content):
-        tab_group = {}
-        max_num_per_row = 1 if self.list_view else max(math.floor(len(self.apps_info) / 2), 4)
-        app_right_click_menu = [[], [E_RUN_APP, E_MOD_APP, E_RMV_APP, E_OPEN_APP_PATH, E_COPY_APP_PATH]]
-        tab_right_click_menu = [[], [E_ADD_TAG, E_RMV_TAG, E_ADD_APP, E_REFRESH]]
-        for tag, apps in self.apps_info.items():
-            tab_layout = []
-            row_layout = []
-            is_filter = True if tag == filter_tag else False
-            for app in apps:
-                col_layout = []
-                app_name = app['name']
-                app_icon = app['icon']
-                app_path = app['path']
-                app_key = app['key']
-                if os.path.isfile(app_icon):
-                    icon = sg.Image(source=app_icon, tooltip=app_name, key='[icon]'+app_key, enable_events=True,
-                                    background_color='white', right_click_menu=app_right_click_menu)
-                    col_layout.append(icon)
-                button = sg.Button(button_text=app_name[:20], tooltip=app_name, key=app_key, size=(20, 1),
-                                   button_color=('black', 'white'), enable_events=False, border_width=0,
-                                   right_click_menu=app_right_click_menu, expand_y=True)
-                col_layout.append(button)
-                if self.list_view:
-                    path = sg.Text(text=app_path[:35], tooltip=app_path, size=(35, 1), justification='left',
-                                   background_color='white', enable_events=True)
-                    col_layout.append(path)
-                    time = sg.Text(app['time'], background_color='white')
-                    col_layout.append(time)
-                    col = sg.Column([col_layout], background_color='white', expand_y=True, expand_x=True,
-                                    right_click_menu=app_right_click_menu, grab=True)
-                else:
-                    col = sg.Column([col_layout], background_color='white')
-                if (is_filter and re.search(filter_content, app_name, re.IGNORECASE)) or not is_filter:
-                    row_layout.append(col)
-                    self.app_keys.append(app_key)
-                if len(row_layout) == max_num_per_row:
-                    tab_layout.append(copy.deepcopy(row_layout))
-                    row_layout.clear()
-            else:
-                if len(row_layout) > 0:
-                    tab_layout.append(row_layout)
-            tab_group[tag] = [[sg.Column(layout=tab_layout, scrollable=True, vertical_scroll_only=True,
-                                         expand_x=True, expand_y=True)]]
-        tab_group_layout = [[sg.Tab(title=os.path.basename(tab_key), layout=tab_layout, key=tab_key,
-                                    right_click_menu=tab_right_click_menu)
-                             for tab_key, tab_layout in tab_group.items()]]
-        return [[sg.TabGroup(tab_group_layout, key='-TAB-GROUP-', expand_x=True, expand_y=True,
-                             right_click_menu=tab_right_click_menu)]]
-
-    def init_window(self):
-        sg.theme(self.theme)
-        show_icon = E_SHOW_ICON if self.list_view else '!' + E_SHOW_ICON
-        show_list = E_SHOW_LIST if not self.list_view else '!' + E_SHOW_LIST
-        menu_def = [[E_FILE, [E_NEW_WINDOW, E_CLOSE_WINDOW, E_IMPORT_APP_INFO, E_EXPORT_APP_INFO, E_QUIT_WITH_HOTKEY]],
-                    [E_EDIT, [E_MANAGE_TAG, E_ADD_TAG]],
-                    [E_VIEW, [show_icon, show_list]],
-                    [E_OPTION, [E_SELECT_ROOT, E_SELECT_THEME, E_SELECT_FONT, E_SET_WINDOW]],
-                    [E_HELP, [E_CHECK_UPDATE, E_ABOUT]]]
-        layout = [[sg.Menu(menu_def, key='-MENU-BAR-')]]
-        col_layout = [[sg.Input(enable_events=False, key='-CONTENT-', expand_x=True, expand_y=True),
-                       sg.Button(E_SEARCH, key='-SEARCH-', bind_return_key=True)]]
-        layout += [[sg.Column(col_layout, expand_x=True)]]
-        filter_tag = self.filter_cond['tab'] if self.filter_cond and len(self.filter_cond) > 0 else ''
-        filter_content = self.filter_cond['content'] if self.filter_cond and len(self.filter_cond) > 0 else ''
-        layout += self.init_window_content(filter_tag, filter_content)
-        col_layout = [[sg.Text('欢迎使用' + self.name, key='-NOTIFICATION-'), sg.Sizegrip()]]
-        layout += [[sg.Column(col_layout, expand_x=True)]]
-        if self.location:
-            self.window = sg.Window(self.name, layout, location=self.location, enable_close_attempted_event=True,
-                                    resizable=True, finalize=True, icon=self.icon, font='Courier 12',
-                                    element_justification='top')
-        else:
-            self.window = sg.Window(self.name, layout, enable_close_attempted_event=True, resizable=True,
-                                    finalize=True, icon=self.icon, font='Courier 12', element_justification='top')
-        # self.window.set_min_size(self.window.size)
-        self.location = self.window.current_location()
-        if filter_tag:
-            self.window[filter_tag].select()
-        if filter_content:
-            self.window['-CONTENT-'].update(filter_content)
-        for key in self.app_keys:
-            self.window[key].bind('<Button-1>', ' LeftClick')
-            self.window[key].bind('<Button-2>', ' MiddleClick')
-            self.window[key].bind('<Button-3>', ' RightClick')
-        self.bind_hotkeys()
-        return self.window
-
-    def bind_hotkeys(self):
-        for event, hotkeys in self.event_hotkeys.items():
-            for hotkey in hotkeys:
-                self.window.bind(hotkey, event)
-
-    def init_systray(self):
-        menu = [[], [E_SHOW, E_HIDE, E_QUIT]]
-        if self.enable_systray and self.window:
-            self.systray = pt.SystemTray(menu, icon=self.icon, window=self.window, tooltip=self.name,
-                                         single_click_events=False)
-        return self.systray
 
     def read(self, timeout=None, timeout_key=sg.TIMEOUT_KEY, close=False):
         event, values = self.window.read(timeout, timeout_key, close)
@@ -437,6 +337,15 @@ class MyApp:
             self.refresh()
         return result
 
+    def sort_app(self, **_kwargs):
+        if self.sort_type == SortType.DEFAULT:
+            self.sort_type = SortType.ASCEND
+        elif self.sort_type == SortType.ASCEND:
+            self.sort_type = SortType.DESCEND
+        elif self.sort_type == SortType.DESCEND:
+            self.sort_type = SortType.DEFAULT
+        self.refresh()
+
     def select_app(self, **kwargs):
         event = kwargs['event']
         key = event.replace('Click', '').replace('Right', '').replace('Left', '').strip()
@@ -605,25 +514,115 @@ class MyApp:
         if os.path.isdir(root):
             self.refresh(root)
 
+    def init_window(self):
+        sg.theme(self.theme)
+        show_icon = E_SHOW_ICON if self.list_view else '!' + E_SHOW_ICON
+        show_list = E_SHOW_LIST if not self.list_view else '!' + E_SHOW_LIST
+        menu_def = [[E_FILE, [E_NEW_WINDOW, E_CLOSE_WINDOW, E_IMPORT_APP_INFO, E_EXPORT_APP_INFO, E_QUIT_WITH_HOTKEY]],
+                    [E_EDIT, [E_MANAGE_TAG, E_ADD_TAG]],
+                    [E_VIEW, [show_icon, show_list, E_SRT_APP, E_REFRESH]],
+                    [E_OPTION, [E_SELECT_ROOT, E_SELECT_THEME, E_SELECT_FONT, E_SET_WINDOW]],
+                    [E_HELP, [E_CHECK_UPDATE, E_ABOUT]]]
+        layout = [[sg.Menu(menu_def, key='-MENU-BAR-')]]
+        col_layout = [[sg.Input(enable_events=False, key='-CONTENT-', expand_x=True, expand_y=True),
+                       sg.Button(E_SEARCH, key='-SEARCH-', bind_return_key=True)]]
+        layout += [[sg.Column(col_layout, expand_x=True)]]
+        filter_tag = self.filter_cond['tab'] if self.filter_cond and len(self.filter_cond) > 0 else ''
+        filter_content = self.filter_cond['content'] if self.filter_cond and len(self.filter_cond) > 0 else ''
+        layout += self.init_window_content(filter_tag, filter_content)
+        col_layout = [[sg.Text('欢迎使用' + self.name, key='-NOTIFICATION-'), sg.Sizegrip()]]
+        layout += [[sg.Column(col_layout, expand_x=True)]]
+        if self.location:
+            self.window = sg.Window(self.name, layout, location=self.location, enable_close_attempted_event=True,
+                                    resizable=True, finalize=True, icon=self.icon, font='Courier 12',
+                                    element_justification='top')
+        else:
+            self.window = sg.Window(self.name, layout, enable_close_attempted_event=True, resizable=True,
+                                    finalize=True, icon=self.icon, font='Courier 12', element_justification='top')
+        self.window.set_min_size((300, 500))
+        self.location = self.window.current_location()
+        if filter_tag:
+            self.window[filter_tag].select()
+        if filter_content:
+            self.window['-CONTENT-'].update(filter_content)
+        self.bind_hotkeys()
+        return self.window
+
+    def init_window_content(self, filter_tag, filter_content):
+        tab_group = {}
+        max_num_per_row = 1 if self.list_view else max(math.floor(len(self.apps_info) / 2), 4)
+        app_right_click_menu = [[], [E_RUN_APP, E_MOD_APP, E_RMV_APP, E_OPEN_APP_PATH, E_COPY_APP_PATH]]
+        tag_right_click_menu = [[], [E_ADD_TAG, E_RMV_TAG, E_SRT_APP, E_ADD_APP, E_REFRESH]]
+        for tag, apps in self.apps_info.items():
+            tab_layout = []
+            row_layout = []
+            is_filter = True if tag == filter_tag else False
+            if self.sort_type == SortType.ASCEND:
+                apps = sorted(apps, key=lambda x: x['name'].upper())
+            elif self.sort_type == SortType.DESCEND:
+                apps = sorted(apps, key=lambda x: x['name'].upper(), reverse=True)
+            for app in apps:
+                col_layout = []
+                app_name = app['name']
+                app_icon = app['icon']
+                app_path = app['path']
+                app_key = app['key']
+                if os.path.isfile(app_icon):
+                    icon = sg.Image(source=app_icon, tooltip=app_name, key='[icon]' + app_key, enable_events=True,
+                                    background_color='white', right_click_menu=app_right_click_menu)
+                    col_layout.append(icon)
+                button = sg.Button(button_text=app_name[:20], tooltip=app_name, key=app_key, size=(20, 1),
+                                   button_color=('black', 'white'), enable_events=False, border_width=0,
+                                   right_click_menu=app_right_click_menu, expand_y=True)
+                col_layout.append(button)
+                if self.list_view:
+                    path = sg.Text(text=app_path[:35], tooltip=app_path, size=(35, 1), justification='left',
+                                   background_color='white', enable_events=True)
+                    col_layout.append(path)
+                    time = sg.Text(app['time'], background_color='white')
+                    col_layout.append(time)
+                    col = sg.Column([col_layout], background_color='white', expand_y=True, expand_x=True,
+                                    right_click_menu=app_right_click_menu, grab=True)
+                else:
+                    col = sg.Column([col_layout], background_color='white')
+                if (is_filter and re.search(filter_content, app_name, re.IGNORECASE)) or not is_filter:
+                    row_layout.append(col)
+                if len(row_layout) == max_num_per_row:
+                    tab_layout.append(copy.deepcopy(row_layout))
+                    row_layout.clear()
+            else:
+                if len(row_layout) > 0:
+                    tab_layout.append(row_layout)
+            tab_group[tag] = [[sg.Column(layout=tab_layout, scrollable=True, vertical_scroll_only=True,
+                                         expand_x=True, expand_y=True)]]
+        tab_group_layout = [[sg.Tab(title=os.path.basename(tab_key), layout=tab_layout, key=tab_key,
+                                    right_click_menu=tag_right_click_menu)
+                             for tab_key, tab_layout in tab_group.items()]]
+        return [[sg.TabGroup(tab_group_layout, key='-TAB-GROUP-', expand_x=True, expand_y=True,
+                             right_click_menu=tag_right_click_menu)]]
+
+    def init_systray(self):
+        menu = [[], [E_SHOW, E_HIDE, E_QUIT]]
+        if self.enable_systray and self.window:
+            self.systray = pt.SystemTray(menu, icon=self.icon, window=self.window, tooltip=self.name,
+                                         single_click_events=False)
+        return self.systray
+
     def init_apps_info(self):
+        if not self.app_icons:
+            self.app_icons = glob.glob(os.path.join(os.getcwd(), 'images', '*.png'))
         apps_info = {ALL_TAG: []}
         for parent, directorys, _files in os.walk(self.root):
             sub_root = parent.replace(self.root + os.path.sep, '')
             if parent != self.root and sub_root.find(os.path.sep) == -1:
                 if parent not in apps_info:
                     apps_info[parent] = []
-
-                def timestamp_to_datetime(time_stamp, format_string="%Y-%m-%d %H:%M:%S"):
-                    time_array = localtime(time_stamp)
-                    str_date = strftime(format_string, time_array)
-                    return str_date
-
                 apps = [{
                     'name': directory,
                     'key': parent + '#' + os.path.join(parent, directory),
                     'path': os.path.join(parent, directory),
-                    'icon': os.path.join(os.getcwd(), 'images', 'robot_' + str(random.randrange(1, 6)) + '.png'),
-                    'time': timestamp_to_datetime(os.path.getmtime(os.path.join(parent, directory))),
+                    'icon': random.choice(self.app_icons),
+                    'time': self.timestamp_to_datetime(os.path.getmtime(os.path.join(parent, directory))),
                 } for directory in directorys]
                 apps_info[parent] += apps
                 apps_info[ALL_TAG] += apps
@@ -653,6 +652,7 @@ class MyApp:
             E_ADD_TAG: self.add_tag,
             E_MOD_TAG: self.mod_tag,
             E_RMV_TAG: self.rmv_tag,
+            E_SRT_APP: self.sort_app,
             E_ADD_APP: self.add_app,
             E_MOD_APP: self.mod_app,
             E_RMV_APP: self.rmv_app,
@@ -686,8 +686,26 @@ class MyApp:
             E_COPY_APP_PATH: ['<Control-c>', '<Control-C>'],
         }
 
+    def bind_hotkeys(self):
+        for tag, apps in self.apps_info.items():
+            for app in apps:
+                key = app['key']
+                self.window[key].bind('<Button-1>', ' LeftClick')
+                self.window[key].bind('<Button-2>', ' MiddleClick')
+                self.window[key].bind('<Button-3>', ' RightClick')
+        for event, hotkeys in self.event_hotkeys.items():
+            for hotkey in hotkeys:
+                self.window.bind(hotkey, event)
+
     @staticmethod
     def set_env(key, value):
         if os.getenv(key) != value:
             command = f'start /b setx {key} {value}'.format(key=key, value=value)
             os.popen(command)
+
+    @staticmethod
+    def timestamp_to_datetime(time_stamp, format_string="%Y-%m-%d %H:%M:%S"):
+        time_array = localtime(time_stamp)
+        str_date = strftime(format_string, time_array)
+        return str_date
+
